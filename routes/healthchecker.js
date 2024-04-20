29f134f86fd0d3d70549e49332de9f9a470acfd0
ashish kumari <ashish.bmninfotech@gmail.com>
2024-04-20 16:21:03 +0530

    first commit

---
+++ routes/healthchecker.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const router=express.Router({});
+
+router.get("/",async (req,res,next)=>{
+    const healthCheck={
+        uptime:process.uptime(),
+        responsetime:process.hrtime(),
+        message: "OK",
+        timestamp : Date.now()
+    }
+    try{
+        res.send(healthCheck);
+    }catch(error){
+        healthCheck.message=error;
+        res.status(503).send();
+    }
+})
+
+module.exports = router;
